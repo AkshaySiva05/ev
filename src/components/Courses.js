5d8620de47531d7b2f7a47d4bca671ad65d17821
AkshaySiva05 <akshaysiva86@gmail.com>
2023-08-13 22:38:12 +0530

    course page details

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -15,7 +15,7 @@ function Courses() {
             </div>
 
 
-            <div className="grid w-full md:grid-rows-2 justify-evenly items-center my-8 p-4">
+            <div className="grid w-full md:flex justify-evenly items-center my-8 p-4">
                 <div className="flex flex-row justify-center">
                     <div className="flex flex-col justify-between">
                         <div className="flex flex-col items-center justify-center">
@@ -28,11 +28,13 @@ function Courses() {
                                 <div className="border-8 border-[#2CDC92] p-2 w-[138px] h-[281px]  bg-white drop-shadow-2xl  origin-bottom-left ">
                                     <div className="flex flex-col items-center">
                                         <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px]"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center">TWO</div>
+                                        <div className="flex flex-roev/blob/akshay/src/components/Courses.jsw bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center">TWO</div>
                                         <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center">YEAR</div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center text-[24px] font-bold text-white items-center">COURSE</div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center text-[23px] font-bold text-white items-center">COURSE</div>
                                         <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center  text-white items-center">
+                                            <button className="">View more</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -45,9 +47,11 @@ function Courses() {
                                         <div className="flex flex-row bg-white w-[111px] h-[35px]"></div>
                                         <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">ONE</div>
                                         <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">YEAR</div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center text-[24px] font-bold text-white items-center">COURSE</div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center text-[23px] font-bold text-white items-center">COURSE</div>
                                         <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center  text-white items-center">
+                                            <button className="">View more</button>
+                                        </div>
 
                                         {/* <div className=""><h1 className="flex flex-row items-center text-2xl font-bold">TWO YEAR COURSE</h1></div> */}
                                         {/* <div className=""><button className=" p-2 text-white"> View more</button></div> */}
@@ -55,32 +59,36 @@ function Courses() {
                                 </div>
                             </div>
 
+                            <div class=" flex flex-col items-center p-2 ">
+                                <div className="bg-[#2CDC92] w-[47px] h-[13px]"></div>
+                                <div className="border-8 border-[#2CDC92] p-2 w-[138px] h-[281px]  bg-white drop-shadow-2xl  origin-bottom-left ">
+                                    <div className="flex flex-col items-center">
+                                        <div className="flex flex-row bg-white w-[111px] h-[35px]"></div>
+                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">ONE</div>
+                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">MONTH</div>
+                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[23px] font-bold text-[#2CDC92] items-center">COURSE</div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center  text-white items-center">
+                                            <button className="">View more</button>
+                                        </div>
+
+                                        {/* <div className=""><h1 className="flex flex-row items-center text-2xl font-bold">TWO YEAR COURSE</h1></div> */}
+                                        {/* <div className=""><button className=" p-2 text-white"> View more</button></div> */}
+                                    </div>
+                                </div>
+                            </div>
                             <div class=" flex flex-col items-center p-2 ">
                                 <div className="bg-[#2CDC92] w-[47px] h-[13px]"></div>
                                 <div className="border-8 border-[#2CDC92] p-2 w-[138px] h-[281px]  bg-white drop-shadow-2xl  origin-bottom-left ">
                                     <div className="flex flex-col items-center">
                                         <div className="flex flex-row bg-white w-[111px] h-[35px]"></div>
                                         <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">SIX</div>
-                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">MONTH</div>
-                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[24px] font-bold text-[#2CDC92] items-center">COURSE</div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
-
-                                        {/* <div className=""><h1 className="flex flex-row items-center text-2xl font-bold">TWO YEAR COURSE</h1></div> */}
-                                        {/* <div className=""><button className=" p-2 text-white"> View more</button></div> */}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class=" flex flex-col items-center p-2 ">
-                                <div className="bg-[#2CDC92] w-[47px] h-[13px]"></div>
-                                <div className="border-8 border-[#2CDC92] p-2 w-[138px] h-[281px]  bg-white drop-shadow-2xl  origin-bottom-left ">
-                                    <div className="flex flex-col items-center">
-                                        <div className="flex flex-row bg-white w-[111px] h-[35px]"></div>
+                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">DAYS</div>
+                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2 justify-center text-[23px] font-bold text-[#2CDC92] items-center">COURSE</div>
                                         <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-white w-[111px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2"></div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[111px] h-[35px] mt-2 justify-center  text-white items-center">
+                                            <button className="">View more</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -101,11 +109,13 @@ function Courses() {
                                 <div className="border-8 border-[#2CDC92] w-[165px] h-[281px] bg-white p-2 drop-shadow-2xl  origin-bottom-left">
                                     <div className="flex flex-col items-center">
                                         <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px]"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center"></div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center">ONE</div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center">WEEK</div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2 justify-center text-[23px] font-bold text-white items-center">TRAINING</div>
                                         <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2"></div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2 justify-center  text-white items-center">
+                                            <button className="">View more</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -115,11 +125,13 @@ function Courses() {
                                 <div className="border-8 border-[#2CDC92] w-[165px] h-[281px] bg-white p-2 drop-shadow-2xl  origin-bottom-left">
                                     <div className="flex flex-col items-center">
                                         <div className="flex flex-row bg-white w-[138px] h-[35px]"></div>
-                                        <div className="flex flex-row bg-white w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center"></div>
-                                        <div className="flex flex-row bg-white w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-white items-center"></div>
-                                        <div className="flex flex-row bg-white w-[138px] h-[35px] mt-2"></div>
+                                        <div className="flex flex-row bg-white w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">ONE</div>
+                                        <div className="flex flex-row bg-white w-[138px] h-[35px] mt-2 justify-center text-[31px] font-bold text-[#2CDC92] items-center">DAY</div>
+                                        <div className="flex flex-row bg-white w-[138px] h-[35px] mt-2 justify-center text-[23px] font-bold text-[#2CDC92] items-center">WORKSHOP</div>
                                         <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2"></div>
-                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2"></div>
+                                        <div className="flex flex-row bg-[#2CDC92] w-[138px] h-[35px] mt-2 justify-center  text-white items-center">
+                                            <button className="">View more</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
